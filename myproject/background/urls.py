f541b8397b5e33b3426bf7f68bebd1d2463afb76
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2024-07-02 19:09:18 +0530

    update

---
+++ myproject/background/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+     path('process_image/', views.upload_image, name='process_image')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myproject/background/views.py
@@ -0,0 +1,31 @@
+import uuid
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from rembg import remove
+
+def upload_image(request):
+    output_image_url = None
+
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        input_path = fs.save(image.name, image)
+        input_path = fs.path(input_path)
+
+        with open(input_path, 'rb') as input_file:
+            input_image = input_file.read()
+
+        # Process the image to remove the background
+        output_image = remove(input_image)
+
+        # Generate a unique filename for the output image
+        unique_filename = f"{uuid.uuid4().hex}.png"
+        output_path = fs.path(unique_filename)
+        with open(output_path, 'wb') as output_file:
+            output_file.write(output_image)
+
+        output_image_url = fs.url(unique_filename)
+
+    return render(request, 'upload.html', {
+        'output_image': output_image_url,
+    })
